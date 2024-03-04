f05bb82b0f1fad9e90a8b4304e168aad8b702f9f
nhtan <hoangtan250997@gmail.com>
2024-03-04 16:06:34 +0700

    first commit

--- src/main/java/com/example/producer/OrderController.java
+++ src/main/java/com/example/producer/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/food")
+@RequestMapping("/${topic.name}")
 public class OrderController {
     private final OrderService orderService;
 
